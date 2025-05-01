ca2f17ccda0a2eec1684d701d5e0dedeb6b3d939
sandhu-saab <sandhusaab61291@gmail.com>
2025-05-01 22:29:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bot import Bot
 import pyrogram.utils
 
-# Set minimum valid channel ID (if needed for forward checks etc.)
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 if __name__ == "__main__":
