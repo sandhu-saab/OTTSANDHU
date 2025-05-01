8e17d080c1ff17ca279625198c241998d119d629
sandhu-saab <sandhusaab61291@gmail.com>
2025-05-01 22:28:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,3 @@
-# Don't Remove Credit @Ott_Sandhu, @Baii_Ji
-# Ask Doubt on telegram @Baii_Ji
-#
-#
-#
-#
-#
-#
-#
-# All rights reserved.
-#
-
 from aiohttp import web
 from plugins import web_server
 import asyncio
@@ -19,141 +7,89 @@ from pyrogram.enums import ParseMode
 import sys
 import pytz
 from datetime import datetime
-#Baii_Ji on Tg
 from config import *
 from database.db_premium import *
 from database.database import *
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import logging
 
-# Suppress APScheduler logs below WARNING level
 logging.getLogger("apscheduler").setLevel(logging.WARNING)
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
 scheduler.add_job(remove_expired_users, "interval", seconds=10)
 
-# Reset verify count for all users daily at 00:00 IST
 async def daily_reset_task():
-    try:
-        await db.reset_all_verify_counts()
-    except Exception:
-        pass  
+    try:
+        await db.reset_all_verify_counts()
+    except Exception:
+        pass
 
 scheduler.add_job(daily_reset_task, "cron", hour=0, minute=0)
 
-
-
-name ="""
- BY CODEFLIX BOTS
-"""
-
-
-
 def get_indian_time():
-    """Returns the current time in IST."""
-    ist = pytz.timezone("Asia/Kolkata")
-    return datetime.now(ist)
-
-
+    ist = pytz.timezone("Asia/Kolkata")
+    return datetime.now(ist)
 
 class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name="Bot",
-            api_hash=API_HASH,
-            api_id=APP_ID,
-            plugins={
-                "root": "plugins"
-            },
-            workers=TG_BOT_WORKERS,
-            bot_token=TG_BOT_TOKEN
-        )
-        self.LOGGER = LOGGER
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={"root": "plugins"},
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
 
+    async def start(self):
+        scheduler.start()
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
 
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Check bot admin rights in DB channel. CHANNEL_ID={CHANNEL_ID}")
+            self.LOGGER(__name__).info("Bot stopped. Contact https://t.me/Baii_Ji for help.")
+            sys.exit()
 
-    async def start(self):
-        scheduler.start()  # Fixed: Start scheduler in running event loop
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.uptime = datetime.now()
+        self.set_parse_mode(ParseMode.HTML)
+        self.username = usr_bot_me.username
 
+        self.LOGGER(__name__).info("Bot Running.. Created by @Baii_Ji")
+        self.LOGGER(__name__).info(r"""
+  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___
+ / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
+| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
+ \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
+        """)
 
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
 
-        try:
-            db_channel = await self.get_chat(CHANNEL_ID)
-            self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
-            await test.delete()
-        except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Contact https://t.me/Baii_Ji for support")
-            sys.exit()
+        try:
+            await self.send_message(OWNER_ID, text="<b><blockquote>Bot Restarted by @Baii_Ji</blockquote></b>")
+        except:
+            pass
 
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
 
-
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-
-
-
-        self.LOGGER(__name__).info(r"""       
-
-
-  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
- / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
-| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
- \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
-                                                         
- 
-                                          """)
-
-
-
-        self.set_parse_mode(ParseMode.HTML)
-        self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Baii_Ji")    
-
-
-
-        # Start Web Server
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
-
-
-
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Baii_Ji</blockquote></b>")
-        except: pass
-
-
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
-
-
-
-    def run(self):
-        """Run the bot."""
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @Baii_Ji")
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            self.LOGGER(__name__).info("Shutting down...")
-        finally:
-            loop.run_until_complete(self.stop())
-
-
-
-# Don't Remove Credit @Ott_Sandhu, @Baii_Ji
-# Ask Doubt on telegram @Baii_Ji
-#
-#
-#
-#
-#
-# All rights reserved.
+    def run(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        self.LOGGER(__name__).info("Bot is now running.")
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            self.LOGGER(__name__).info("Shutting down...")
+        finally:
+            loop.run_until_complete(self.stop())
