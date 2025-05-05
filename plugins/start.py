75b4bad0121359f5c36b9f68657bb10e58efe882
sandhu-saab <sandhusaab61291@gmail.com>
2025-05-05 06:48:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -286,7 +286,7 @@ async def not_joined(client: Client, message: Message):
                                 expire_date=datetime.utcnow() + timedelta(seconds=FSUB_LINK_EXPIRY) if FSUB_LINK_EXPIRY else None)
                             link = invite.invite_link
 
-                    buttons.append([InlineKeyboardButton(text=name, url=link)])
+                    buttons.append([InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=link)])
                     count += 1
                     await temp.edit(f"<b>{'! ' * count}</b>")
 
