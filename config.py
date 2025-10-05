e0f434b72b54578c359ee3b3457718035dfd4f68
sandhu-saab <sandhusaab61291@gmail.com>
2025-10-05 08:07:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,68 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Bot Configuration
+LOG_FILE_NAME = "bot.log"
+PORT = '5010'
+OWNER_ID = 6157414954
+
+MSG_EFFECT = 5046509860389126442
+
+SHORT_URL = "linkshortify.com" # shortner url 
+SHORT_API = "d69bdc9eeef4d1cb0d2bb9733099a0282bdc64ac" 
+SHORT_TUT = "https://t.me/+dVRLYHXJztJlMmY9"
+
+# Bot Configuration
+SESSION = "yato"
+TOKEN = ""
+API_ID = "29882686"
+API_HASH = "b642a25aee67b2aed02116df4a916bca"
+WORKERS = 5
+
+DB_URI = "mongodb+srv://sukhwinder020202:store1234@cluster0.rzrwnui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "sukhwinder020202"
+
+FSUBS = [[-1002429510787, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+# Database Channel (Primary)
+DB_CHANNEL =-1002253089880    # just put channel id dont add ""
+# Multiple Database Channels (can be set via bot settings)
+# DB_CHANNELS = {
+#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
+#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+# }
+# Auto Delete Timer (seconds)
+AUTO_DEL = 300
+# Admin IDs
+ADMINS = [6157414954, 6316008361, 6796307271]
+# Bot Settings
+DISABLE_BTN = True
+PROTECT = False
+
+# Messages Configuration
+MESSAGES = {
+    "START": "<b>›› ʜᴇʏ!!, {first} ~ <blockquote>ʟᴏᴠᴇ ᴘᴏʀɴʜᴡᴀ? ɪ ᴀᴍ ᴍᴀᴅᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ғɪɴᴅ ᴡʜᴀᴛ ʏᴏᴜ aʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ.</blockquote></b>",
+    "FSUB": "<b><blockquote>›› ʜᴇʏ ×</blockquote>\n  ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b>",
+    "ABOUT": "<b>›› ғᴏʀ ᴍᴏʀᴇ: @OttSandhu\n <blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/OttSandhu'>Cʟɪᴄᴋ ʜᴇʀᴇ</a> \n›› ᴏᴡɴᴇʀ: @Baii_Ji\n›› ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a> \n›› ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a> \n›› ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a> \n›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @Baii_Ji</b></blockquote>",
+    "REPLY": "<b>For More Join - @OttSandhu</b>",
+    "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
+    "START_PHOTO": "https://graph.org/file/510affa3d4b6c911c12e3.jpg",
+    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
+}
+
+def LOGGER(name: str, client_name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter(
+        f"[%(asctime)s - %(levelname)s] - {client_name} - %(name)s - %(message)s",
+        datefmt='%d-%b-%y %H:%M:%S'
+    )
+    file_handler = RotatingFileHandler(LOG_FILE_NAME, maxBytes=50_000_000, backupCount=10)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
